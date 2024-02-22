7d3068c4821cbade4b0db2678f90296704e41feb
goodchuck <y502100@naver.com>
2024-02-22 21:14:34 +0900

    feat : 사이드바 추가

---
+++ src/app/_component/SideBar.tsx
@@ -0,0 +1,93 @@
+"use client"
+import React, { useState } from 'react';
+import {
+    AppstoreOutlined,
+    ContainerOutlined,
+    DesktopOutlined,
+    MailOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
+import { useRouter, usePathname } from 'next/navigation';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    // getItem('audio', '1', <PieChartOutlined />),
+    // getItem('image', '2', <DesktopOutlined />),
+    // getItem('Option 3', '3', <ContainerOutlined />),
+
+    getItem('Audio', '/audio', <MailOutlined />, [
+        getItem('create-speech', '/audio/create-speech'),
+        // getItem('Option 6', '6'),
+        // getItem('Option 7', '7'),
+        // getItem('Option 8', '8'),
+    ]),
+
+    getItem('Image', '/image', <AppstoreOutlined />, [
+        getItem('generator', '/image/generator'),
+        getItem('edit', '/image/edit'),
+
+        // getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+    ]),
+    getItem('text', '/text', <AppstoreOutlined />),
+];
+
+export const SideBar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const router = useRouter();
+
+    const pathname = usePathname();
+    const onClick: MenuProps['onClick'] = (e) => {
+        console.log('click ', e);
+        console.log(pathname);
+        if (e.key === 'home') {
+            router.replace('/')
+        } else if (pathname === e.key) {
+
+        }
+        else {
+            router.replace(e.key)
+        }
+
+    };
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
+
+    return (
+        <div style={{ width: 256, height: '100dvh' }}>
+            <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+                {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            </Button>
+            <Menu
+                // defaultSelectedKeys={['1']}
+                // defaultOpenKeys={['sub1']}
+                mode="inline"
+                // theme="dark"
+                inlineCollapsed={collapsed}
+                onClick={onClick}
+                items={items}
+            />
+        </div>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SideBar } from "./_component/SideBar";
 // import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{ display: 'flex', margin: 0 }}>
+        <SideBar></SideBar>
+        {children}
+      </body>
     </html>
   );
 }
