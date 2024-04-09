4cab8f074a4902ffdf94875f645f6ef0b643340a
goodchuck <y502100@naver.com>
2024-04-09 11:30:10 +0900

    design : footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
             <SideBar></SideBar>
             <Flex gap={'middle'} align="start" vertical style={{ width: '84vw', padding: '20px' }}>
               {children}
+              <footer>
+                <p>해당 프로젝트는 포트폴리오 용으로 완벽하지 않을 수 있습니다!</p>
+                <p>만약 문의가 필요하시다면 아래 메일로 연락주시길 바랍니다!</p>
+                <p>문의 메일 : goodchuck852@gmail.com</p>
+              </footer>
             </Flex>
           </StyledComponentsRegistry>
         </RQProvider>
